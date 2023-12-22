c9d4efc7bd23d71543877ff07e14c18f09b70796
Bea Tavernaro <94928593+beatavernaro@users.noreply.github.com>
2023-12-22 16:42:40 -0300

    add desafio POO

---
+++ .NET - Avanade/Desafio - Celular/Program.cs
@@ -0,0 +1,5 @@
+﻿Smartphone iphone = new Iphone("123-456", "Iphone 15", "111222333", 5);
+Smartphone nokia = new Nokia("235-587", "Nokia 1", "5588999666", 8);
+
+iphone.InstalarAplicativo("Notion");
+nokia.InstalarAplicativo("Candy Crush");
