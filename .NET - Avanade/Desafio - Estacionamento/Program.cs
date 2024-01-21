1060297c4d167cadfd2b04af167c43e25c3e9e14
Bea Tavernaro <94928593+beatavernaro@users.noreply.github.com>
2024-01-21 18:31:03 -0300

    Refatorando a classe Veiculo

--- .NET - Avanade/Desafio - Estacionamento/Program.cs
+++ .NET - Avanade/Desafio - Estacionamento/Program.cs
@@ -3,29 +3,33 @@
     public static void Main(string[] args)
     {
         int opt = 0;
-        Console.WriteLine("== Desafio Estacionamento - DIO Decola Tech 2024 ==");
-        Veiculo veiculo = new Veiculo();
+        Veiculo veiculo = new();
 
         do
         {
+            Console.WriteLine("== Desafio Estacionamento - DIO Decola Tech 2024 ==");
             Console.WriteLine("Escolha uma opção: ");
             Console.WriteLine("1 - Adicionar veículo \n2 - Remover veículo \n3 - Listar veículos \n4 - Encerrar");
-            opt = int.Parse(Console.ReadLine());
+            int.TryParse(Console.ReadLine(), out opt);
             switch (opt)
             {
                 case 1:
-                veiculo.AdicionarVeiculo();
+                    veiculo.AdicionarVeiculo();
                     break;
                 case 2:
-                veiculo.RemoverVeiculo();
+                    veiculo.RemoverVeiculo();
                     break;
                 case 3:
-                veiculo.ListarVeiculos();
+                    veiculo.ListarVeiculos();
                     break;
                 case 4:
                     break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    veiculo.Limpar(1000);
+                    break;
             }
-
+            veiculo.Limpar(2000);
         } while (opt != 4);
 
     }
--- .NET - Avanade/Desafio - Estacionamento/Veiculo.cs
+++ .NET - Avanade/Desafio - Estacionamento/Veiculo.cs
@@ -1,36 +1,77 @@
-public class Veiculo{
-    private List<string> listaCarros = new List<string>();
+public class Veiculo
+{
+    private List<string> listaCarros = [];
     private decimal valorFixo = 10.00M;
     private decimal valorHora = 2.00M;
 
-    public void AdicionarVeiculo(){
+    public void AdicionarVeiculo()
+    {
         Console.WriteLine("Digite a placa do veiculo: ");
-        listaCarros.Add(Console.ReadLine());
-        Console.WriteLine("Veiculo adicionado com sucesso! ");
-        Thread.Sleep(2000);
-        Console.Clear();
+        string? novaPlaca = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(novaPlaca))
+        {
+            Console.WriteLine("Digite uma placa válida!");
+        }
+        else if (listaCarros.Contains(novaPlaca))
+        {
+            Console.WriteLine("Veiculo já estacionado!");
+        }
+        else
+        {
+            listaCarros.Add(novaPlaca);
+            Console.WriteLine("Veiculo adicionado com sucesso!");
+        }
     }
 
-    public void RemoverVeiculo(){
+    public void RemoverVeiculo()
+    {
         Console.WriteLine("Digite a placa do veiculo: ");
-        string placa = Console.ReadLine();
-        Console.WriteLine("Digite a quantidade de horas: ");
-        int horas = int.Parse(Console.ReadLine());
-        Console.WriteLine(CalculaValor(horas));
-        listaCarros.Remove(placa);
-        Thread.Sleep(3000);
+        string? placa = Console.ReadLine();
+        if (listaCarros.Contains(placa))
+        {
+            Console.WriteLine("Digite a quantidade de horas: ");
+            if (!int.TryParse(Console.ReadLine(), out int horas))
+            {
+                Console.WriteLine("Digite um número válido!");
+                return;
+            }
+
+            Console.WriteLine(CalculaValor(horas));
+            listaCarros.Remove(placa);
+        }
+        else
+        {
+            Console.WriteLine("Veículo não encontrado!");
+        }
     }
 
-    public string CalculaValor(int horas){
+    public string CalculaValor(int horas)
+    {
         decimal valorTotal = valorFixo + (horas * valorHora);
-        return $"Valor Total: {valorTotal}";
+        return $"Valor Total: {valorTotal.ToString("c")}";
+
     }
 
-    public void ListarVeiculos(){
-        foreach(string veiculo in listaCarros){
+    public void ListarVeiculos()
+    {
+        if (listaCarros.Count == 0)
+        {
+            Console.WriteLine("Não há veículos cadastrados! ");
+            return;
+        }
+
+        foreach (string veiculo in listaCarros)
+        {
             Console.WriteLine(veiculo);
         }
         Console.WriteLine("Digite qualquer tecla para voltar.");
         Console.ReadKey();
+    }
+
+    public void Limpar(int tempo)
+    {
+        Thread.Sleep(tempo);
+        Console.Clear();
     }
 }
