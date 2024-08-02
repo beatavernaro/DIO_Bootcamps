ac24e5bb8bfe48ea89b8e780fd7f0d441ae00a09
Beatriz Tavernaro Cardoso <b.tavernaro.cardoso@avanade.com>
2024-08-02 14:01:30 -0300

    XP Lab Hotel

---
+++ XP_FullStack/ReservaHotelDIO/Models/Reserva.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaHotelDIO.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite? Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva(int diasReservados)
+        {
+            this.DiasReservados = diasReservados;
+        }
+        public void CadastrarHospede(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count > Suite.Capacidade)
+            {
+                Console.WriteLine("A quantidade de hospedes é maior que a capacidade da suite");
+                return;
+            }
+            Hospedes = hospedes;
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            this.Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal totalDiarias = Suite.ValorDiaria;
+            if (DiasReservados >= 10)
+            {
+                totalDiarias *= 0.9m;
+            }
+            return totalDiarias * DiasReservados;
+        }
+    }
+}
---
+++ XP_FullStack/ReservaHotelDIO/Program.cs
@@ -0,0 +1,18 @@
+﻿using ReservaHotelDIO.Models;
+
+List<Pessoa> hospedes = [];
+
+Pessoa p1 = new(nome: "Hospede 1");
+Pessoa p2 = new(nome: "Hospede 2");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+Suite suite = new(tipoSuite: "Premium", capacidade: 3, valorDiaria: 30);
+
+Reserva reserva1 = new(diasReservados: 5);
+reserva1.CadastrarSuite(suite);
+reserva1.CadastrarHospede(hospedes);
+
+Console.WriteLine($"Hospedes: {reserva1.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor da diaria: {reserva1.CalcularValorDiaria()}");
